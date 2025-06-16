93b0d369d827f7afd8e32020d3a1709643ac0606
Sebastian Salgado <git@cebas.com.ar>
2025-06-16 16:42:00 -0300

    add errors and log

---
+++ util/errors.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"errors"
+	"os"
+)
+
+var logger = NewLog(0)
+
+func FatalErrorCheck(err error) {
+	if err != nil {
+		logger.Printf(0, "\n%s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+func Assert(assertion bool, msg string) {
+	if !assertion {
+		FatalErrorCheck(errors.New("assertion failed: " + msg))
+	}
+}
+
+func WarningErrorCheck(err error) bool {
+	if err != nil {
+		logger.Printf(0, "\n%s\n", err.Error())
+		return false
+	}
+
+	return true
+}
---
+++ util/log.go
@@ -0,0 +1,66 @@
+package util
+
+import "fmt"
+
+type Log struct {
+	verbose int
+	indent  int
+}
+
+func NewLog(verbose int) Log {
+	return Log{verbose: verbose, indent: 0}
+}
+
+func _print(msg string) {
+	fmt.Print(msg)
+}
+
+func _println(msg string) {
+	fmt.Println(msg)
+}
+
+func _printf(format string, v ...any) {
+	fmt.Printf(format, v...)
+}
+
+func (l *Log) printIndent() {
+	for i := 0; i < l.indent; i++ {
+		_print("\t")
+	}
+}
+
+func (l *Log) Indent(newLevel int) {
+	l.indent = newLevel
+}
+
+func (l *Log) shouldPrint(verbose int) bool {
+	return verbose <= l.verbose
+}
+
+// Println prints a message to stdout
+func (l *Log) Println(verbose int, msg string) {
+	if l.shouldPrint(verbose) {
+		l.printIndent()
+		_println(msg)
+	}
+}
+
+// Printf prints a formatted message to stdout
+func (l *Log) Printf(verbose int, format string, v ...any) {
+	if l.shouldPrint(verbose) {
+		l.printIndent()
+		_printf(format, v...)
+	}
+}
+
+// Logln prints a message to stdout
+// Deprecated: use Log.Println instead
+func Logln(msg string) {
+	_println(msg)
+}
+
+// Logf prints a formatted message to stdout
+// Deprecated: use Log.Printf instead
+func Logf(format string, v ...any) {
+	_printf(format, v...)
+}
